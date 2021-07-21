d89d0ef86ad10e9de5569fef8554408e01b778b2
tansubaktiran <87576344+tansubaktiran@users.noreply.github.com>
2021-07-21 22:14:53 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,8 +8,4 @@ def main():
     script=server_document("http://graphsmart2.herokuapp.com/app")
     return render_template('flaskbokeh1.html',bokS=script)
 
-if __name__ == "__main__":
-    app.run()
-
-
 #bokeh serve serv_1.py --allow-websocket-origin=127.0.0.1:5000
